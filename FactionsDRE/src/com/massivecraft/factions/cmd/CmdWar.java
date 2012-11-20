a6f2dcaf3e178d63719361b44e4ee36f9098a98e
Pascal <Pascal@10.0.1.36>
2012-11-20 16:10:40 +0100

    Changed the "isNormal()" method

--- FactionsDRE/src/com/massivecraft/factions/cmd/CmdWar.java
+++ FactionsDRE/src/com/massivecraft/factions/cmd/CmdWar.java
@@ -46,7 +46,7 @@ public class CmdWar extends FCommand{
 			
 			
 			if(argFaction!=null){
-				if(argFaction.isNormal() && (!argFaction.getTag().equalsIgnoreCase("Neutral"))){
+				if(argFaction.isNormal()){
 					boolean isAttacker = FWar.get(fme.getFaction(),argFaction)!=null;
 					boolean isTarget = FWar.get(argFaction,fme.getFaction())!=null;
 					
